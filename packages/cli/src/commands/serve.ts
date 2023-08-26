ace9a369f8780bdb35c6e209232a28c53cbc8194
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-26 23:47:47 +0700

    complete building the cli

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { Command } from "commander";
 
 import { serve } from "local-api";
@@ -7,6 +8,8 @@ export const serveCommand = new Command()
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
   .action((filename = "notebook.js", options: { port: string }) => {
+    const dir = path.join(process.cwd(), path.dirname(filename));
+
     console.log("Getting ready to serve a file");
-    serve(parseInt(options.port), filename, "/");
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
