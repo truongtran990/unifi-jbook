55b1d29a1435277d02ae54e9c1c5c0288e59505a
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-26 23:36:18 +0700

    communicate from cli to api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,10 +1,12 @@
 import { Command } from "commander";
 
+import { serve } from "local-api";
+
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options) => {
+  .action((filename = "notebook.js", options: { port: string }) => {
     console.log("Getting ready to serve a file");
-    console.log(filename, options);
+    serve(parseInt(options.port), filename, "/");
   });
