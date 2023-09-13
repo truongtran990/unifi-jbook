21bb24c907313242b0691b976dbc04b7fbc4d4b1
VanTruongUIT <tvtruong.uit@gmail.com>
2023-09-13 23:21:16 +0700

    add description when run cli successfully

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -21,6 +21,10 @@ export const serveCommand = new Command()
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
       await serve(parseInt(options.port), path.basename(filename), dir);
+
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      );
     } catch (error) {
       if (isLocalApiError(error)) {
         if (error.code === "EADDRINUSE") {
