2f5170380700f8cefc3121cdbb5adee7987dad52
VanTruongUIT <tvtruong.uit@gmail.com>
2023-09-13 23:12:20 +0700

    handle exception in cli

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -3,13 +3,34 @@ import { Command } from "commander";
 
 import { serve } from "local-api";
 
+interface LocalApiError {
+  code: string;
+}
+
+const isLocalApiError = (err: any): err is LocalApiError => {
+  return typeof err.code === "string";
+};
+
 export const serveCommand = new Command()
+  // [] is the optional argument
   .command("serve [filename]")
   .description("Open a file for editing")
+  // <> is the required argument
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-
-    console.log("Getting ready to serve a file");
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+    } catch (error) {
+      if (isLocalApiError(error)) {
+        if (error.code === "EADDRINUSE") {
+          console.error(
+            `Port: ${options.port} is in use. Try running on a different port.`
+          );
+        }
+      } else if (error instanceof Error) {
+        console.log("Here's the problem", error.message);
+      }
+      process.exit(1);
+    }
   });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -3,9 +3,7 @@ import express from "express";
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
-  app.listen(port, () => {
-    console.log("serving traffic on port: ", port);
-    console.log("saving/fetching celsl from: ", filename);
-    console.log("that file is in dir: ", dir);
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
   });
 };
