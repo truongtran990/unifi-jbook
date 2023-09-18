4e2a45ce95885966c81611569e09df0af24c2138
VanTruongUIT <tvtruong.uit@gmail.com>
2023-09-18 22:57:54 +0700

    add local-client dependenci for local-api

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -4,13 +4,15 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
-  app.use(
-    createProxyMiddleware({
-      target: "http://127.0.0.1:3000",
-      ws: true,
-      logLevel: "error",
-    })
-  );
+  // app.use(
+  //   createProxyMiddleware({
+  //     target: "http://127.0.0.1:3000",
+  //     ws: true,
+  //     logLevel: "error",
+  //   })
+  // );
+
+  app.use(express.static("../../local-client/build"));
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
