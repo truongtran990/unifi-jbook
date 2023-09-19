0a70473ed8b6fae7ec95aa66535b30a66dc4406c
VanTruongUIT <tvtruong.uit@gmail.com>
2023-09-19 21:20:25 +0700

    add parameter for serve function to determine use proxy or build file

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -2,22 +2,29 @@ import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import path from "path";
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
 
-  // app.use(
-  //   createProxyMiddleware({
-  //     target: "http://127.0.0.1:3000",
-  //     ws: true,
-  //     logLevel: "error",
-  //   })
-  // );
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: "http://127.0.0.1:3000",
+        ws: true,
+        logLevel: "error",
+      })
+    );
+  } else {
+    const packagePath = require.resolve("local-client/build/index.html");
 
-  const packagePath = require.resolve("local-client/build/index.html");
+    console.log("packagePath: ", packagePath);
 
-  console.log("packagePath: ", packagePath);
-
-  app.use(express.static(path.dirname(packagePath)));
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
